491bc587e50680fa7909c3d5189fd55dff39cf6c
Kurt Van Etten <kurt@vanetten.us>
2019-12-19 21:37:00 -0500

    Completed code-along lesson

---
+++ migrations/20191219205113_fruits.js
@@ -0,0 +1,16 @@
+exports.up = async function(knex) {
+  await knex.schema.createTable('fruits', table => {
+    // table.integer('id').notNull().unique().primary()
+    table.increments('id');
+    table
+      .text('name')
+      .notNull()
+      .unique();
+    table.float('avgWeightOz');
+    table.boolean('delicious').defaultTo(false);
+  });
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('fruits');
+};
